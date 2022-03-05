5844aee2c5475044cbc30ffcdf066ab5c7318db4
samuca <augustto.sam3@gmail.com>
2022-03-05 12:02:32 -0300

    commit inicial

---
+++ src/components/major/Main.js
@@ -0,0 +1,17 @@
+import React,{useState} from 'react'
+
+import Paisagem01 from '../imgs/paisagem01.jpg';
+import Paisagem02 from '../imgs/paisagem02.jpg';
+
+
+export default function Main () {
+
+    return (
+
+        <>
+
+        </>
+
+    )
+
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './main.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function ticTac () {
+
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+  
+}
+
+setInterval(ticTac,1000)
+ticTac()
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
